d8b78d8e6f712b1f93be94a0b661bfe4acbf6030
Sanjay Yadav <dhananjayyadav1807@gmail.com>
2025-12-31 15:45:49 +0530

    commit for deployment tesing

--- components/VideoPlayerModal.jsx
+++ components/VideoPlayerModal.jsx
@@ -14,7 +14,6 @@ export default function VideoPlayerModal({ isOpen, onClose, videoId, title }) {
         }
     }, [isOpen]);
 
-
     if (!isOpen && !animate) return null;
 
     // Construct YouTube embed URL
